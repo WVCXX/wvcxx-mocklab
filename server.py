1aa588b57eecf45ca848b970f423c31cbe509da8
WVCXX <igalus00@gmail.com>
2026-04-02 14:39:19 +0300

    Добавлена отдача статики

--- server.py
+++ server.py
@@ -123,5 +123,10 @@ async def mock_handler(request: Request, path: str):
         status_code = 404,
         content = {'error': f"No mock endpoint found for {method} {path}"}
     )
+if os.path.exists("index.html"):
+    app.mount("/", StaticFiles(directory=".", html=True), name="static")
+
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
